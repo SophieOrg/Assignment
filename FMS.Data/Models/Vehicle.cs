cffc3a8988815204f336a10e4ac6e20e63327daa
Sophie Black <Black-s31@ulster.ac.uk>
2022-04-06 11:41:19 +0100

    Assignment original

---
+++ FMS.Data/Models/Vehicle.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FMS.Data.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        
+        // suitable vehicle properties/relationships
+
+    }
+
+}
